47d79f5644e7a9b84187151f6d7efde4f3ab344a
Adam Newhouser <adamnewhouser@gmail.com>
2019-11-01 12:49:14 -0400

    prep for heroku deploy

--- server.js
+++ server.js
@@ -1,11 +1,54 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const POKEDEX = require('./pokedex.json');
 const app = express();
-app.use(morgan('dev'));
-app.use((req, res) => {
-    res.send('Hello, world!')
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
+app.use(helmet());
+app.use(cors());
+
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+    next()
 })
 
-app.listen(8000, () => {
-    console.log('Server listening at http://localhost:8000')
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`];
+
+app.get('/types', function handleGetTypes(req, res) {
+    res.json(validTypes)
+})
+
+app.get('/pokemon', function handleGetPokemon(req, res) {
+    const { name, type } = req.query
+    let response = POKEDEX.pokemon
+    if (name) {
+        response = response.filter(pokemon => pokemon.name.toLowerCase().includes(name.toLowerCase()))
+    }
+    if (type) {
+        response = response.filter(pokemon => pokemon.type.includes(type))
+    }
+    res.json(response)
+})
+
+app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+        response = { error: { message: 'server error' } }
+    } else {
+        response = { error }
+    }
+    res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000
+app.listen(PORT, () => {
+
 })
